1ba558e70d4c95e5b440c968fc7c20b25f6da4b5
rn-santos227 <rn_santos227@yahoo.com>
2024-03-23 22:11:28 +0800

    update color.py class

---
+++ class/Color.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class Color(Enum):
+  BLACK = '\033[30m'
+  RED = '\033[31m'
+  GREEN = '\033[32m'
+  YELLOW = '\033[33m'
+  BLUE = '\033[34m'
+  MAGENTA = '\033[35m'
+  CYAN = '\033[36m'
+  WHITE = '\033[37m'
+  LIGHT_BLACK = '\033[90m'
+  LIGHT_RED = '\033[91m'
+  LIGHT_GREEN = '\033[92m'
+  LIGHT_YELLOW = '\033[93m'
+  LIGHT_BLUE = '\033[94m'
+  LIGHT_MAGENTA = '\033[95m'
+  LIGHT_CYAN = '\033[96m'
+  LIGHT_WHITE = '\033[97m'
+  RESET = '\033[0m'
