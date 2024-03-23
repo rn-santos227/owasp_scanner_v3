3139a748ac89a57bc12e5afd610081960d842d12
rn-santos227 <rn_santos227@yahoo.com>
2024-03-23 21:42:18 +0800

    updated validate method function in validate_method.py

--- utils/validate_method.py
+++ utils/validate_method.py
@@ -1,7 +1,7 @@
 import argparse
 
 def validate_method(value):
-  methods = value.lower()
-  if methods not in ['GET','POST','PUT','PATCH','DELETE']: 
-    raise argparse.ArgumentTypeError(f"argument -m/--method: invalid choice: '{methods}' (choose from 'GET', 'POST', 'PATCH', 'PUT', 'DELETE')")
-  return methods
+  method = value.lower()
+  if method not in ['get','post','put','patch','delete']: 
+    raise argparse.ArgumentTypeError(f"argument -m/--method: invalid choice: '{method}' (choose from 'GET', 'POST', 'PATCH', 'PUT', 'DELETE')")
+  return method
