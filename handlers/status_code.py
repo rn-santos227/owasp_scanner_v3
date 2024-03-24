ecb0f172972e8f4bcdb10e9572d28393387a78c0
rn-santos227 <rn_santos227@yahoo.com>
2024-03-24 20:39:42 +0800

    update status code function in handlers

--- handlers/status_code.py
+++ handlers/status_code.py
@@ -6,3 +6,15 @@ def handle_status_code(status_code):
   global ignore_status_code
   if (status_code // 100 == 4 or status_code // 100 == 5) and ignore_status_code == 0:
     color.warning(f"Response status code is {status_code}")
+    while True:
+      answer = input("Continue? y/n: ")
+
+      if answer.lower() not in ['y','n']:
+        color.warning("Invalid Input.")
+        continue
+
+      if answer.lower() == 'y':
+        ignore_status_code = 1 
+      else:
+        print("SYSTEM CLOSING. BYE.")
+        exit(0)
