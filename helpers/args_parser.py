3dc7e1b1cdabdaea3fbdd635e4468d248fe832c4
rn-santos227 <rn_santos227@yahoo.com>
2024-03-24 20:46:48 +0800

    update files in helpers

--- helpers/args_parser.py
+++ helpers/args_parser.py
@@ -1,7 +1,10 @@
+import helpers.color_text as color
+
 def read(file: str):
   try:
     with open(file) as f:
       payloads = f.readlines()
   except Exception as err:
-    print(err)
+    message = color.warning(err)
+    print(message)
   return payloads
