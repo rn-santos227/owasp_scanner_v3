cfc0f092741297e649c79cd4ecba631a166b2325
rn-santos227 <rn_santos227@yahoo.com>
2024-03-24 20:15:30 +0800

    update file writer function

--- helpers/file_writer.py
+++ helpers/file_writer.py
@@ -1,10 +1,14 @@
 import os
 
-def file_writer(file : str):
+def file_writer(file_name : str, text):
   try:
     folder_path = "files"
     if not os.path.exists(folder_path):
       os.makedirs(folder_path)
-      
+
+    file_path = os.path.join(folder_path, file_name)
+    with open(file_path, 'a') as file:
+      file.write(str(text) + '\n')
+
   except Exception as err:
     print(f"An error occurred: {err}")
