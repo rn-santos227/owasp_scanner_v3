32a7f8a1e12fb2ecdf8651b1f9624a3ffe46db3c
rn-santos227 <rn_santos227@yahoo.com>
2024-03-25 20:28:50 +0800

    update response handler function

--- handlers/response.py
+++ handlers/response.py
@@ -1,8 +1,17 @@
+import utils.format_text as formatter
 import requests;
 
-def process_response(request : requests, match_string, default_testing_length, verbose):
+
+def handle_response(request : requests, match_string, default_testing_length, verbose):
   response = request.text
   if match_string is not None:
     for string in match_string:
       if string in response:
-         messaage = f"[+] {request.request.method} - \"{str(string)}\" detected: {request.request.url}"
+        print(f"[+] {request.request.method} - \"{str(string)}\" detected: {request.request.url}")
+      
+      if len(response) != default_testing_length:
+        print(f"[+] {request.request.method} - Different response length: {str(len(response))} - {request.request.url} ")
+
+      if verbose:
+        print(f"[VERBOSE] {request.request.method} {str(len(response))}       {request.request.url}")
+         
