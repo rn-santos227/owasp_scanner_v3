1da277a8ea9766f39d0e7963e90b6475b6fe98a8
rn-santos227 <rn_santos227@yahoo.com>
2024-03-26 11:13:35 +0800

    added owasp api 5 function

---
+++ modules/OWASP_api5.py
@@ -0,0 +1,3 @@
+#API5:2023 - Broken Function Level Authorization
+def check_api_5(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None):
+  pass
