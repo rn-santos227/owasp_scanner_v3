034cb9b6ea407ff1666588abf1fa7eb782665e75
rn-santos227 <rn_santos227@yahoo.com>
2024-03-26 11:06:56 +0800

    added owasp api 1 function

---
+++ modules/OWASP_api1.py
@@ -0,0 +1,3 @@
+#API1:2023 - Broken Object Level Authorization
+def check_api_1(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None):
+  pass
