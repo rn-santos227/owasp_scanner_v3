7b991f8dc79283ebe6b29225e8a3d19beef216db
rn-santos227 <rn_santos227@yahoo.com>
2024-03-26 13:31:16 +0800

    update main.py, added key handler section in main

--- main.py
+++ main.py
@@ -18,3 +18,18 @@ def main(stdscr):
       stdscr.refresh()
 
       key = stdscr.getch()
+      
+      if key == curses.KEY_UP:
+        selected_option = (selected_option - 1) % len(options)
+      
+      elif key == curses.KEY_DOWN:
+        selected_option = (selected_option + 1) % len(options)
+      
+      elif key == curses.KEY_ENTER or key in [10, 13]:
+        if selected_option == len(options) - 1:
+          print("Exiting Program... Bye.")
+          break
+        else:
+          print(f"You selected: {options[selected_option]}")
+          stdscr.getch()
+          
