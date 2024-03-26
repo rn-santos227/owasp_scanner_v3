80c7aecf5817d14470c420479e7cb196a4035dcc
rn-santos227 <rn_santos227@yahoo.com>
2024-03-26 13:27:27 +0800

    update main.py, added key input getter

--- main.py
+++ main.py
@@ -7,4 +7,14 @@ def main(stdscr):
 
   options = ["Option 1", "Option 2", "Option 3", "Exit"]
   selected_option = 0
-  pass
+
+  while True:
+    stdscr.clear()
+    for index, option in enumerate(options):
+      if index == selected_option:
+        stdscr.addstr(index, 0, option, curses.A_REVERSE)
+      else:
+        stdscr.addstr(index, 0, option)
+      stdscr.refresh()
+
+      key = stdscr.getch()
