229139404657e21fc6d8583b87c56af2a5c4a166
rn-santos227 <rn_santos227@yahoo.com>
2024-03-26 13:24:02 +0800

    update main.py, added cls option selection

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-def main():
+import curses
+
+def main(stdscr):
+  curses.curs_set(0)
+  stdscr.clear()
+  stdscr.refresh()
+
+  options = ["Option 1", "Option 2", "Option 3", "Exit"]
+  selected_option = 0
   pass
