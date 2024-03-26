fa65f3dd93318ff7123689e352f62ffb1c47e76f
rn-santos227 <rn_santos227@yahoo.com>
2024-03-26 11:11:58 +0800

    added owasp api 4 function

---
+++ modules/OWASP_api4.py
@@ -0,0 +1,3 @@
+#API4:2023 - Unrestricted Resource Consumption
+def check_api_4(endpoint, method : str, headers: dict, timeout : float, verbose : bool = False, data : str = None, json : dict = None):
+  pass
