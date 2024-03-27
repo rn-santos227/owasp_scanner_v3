d5859142e49622f6260f526073556e0a229c49f2
rn-santos227 <rn_santos227@yahoo.com>
2024-03-27 19:04:23 +0800

    update owasp api 10 function

--- modules/OWASP_api10.py
+++ modules/OWASP_api10.py
@@ -1,3 +1,3 @@
 #API10:2023 - Unsafe Consumption of APIs
-def check_api_10(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None):
+def check_api_10(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
  pass
