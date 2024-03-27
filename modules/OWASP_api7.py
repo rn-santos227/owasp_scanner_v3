3e33c1c907355560e0fdabb745d4d8af03191f1d
rn-santos227 <rn_santos227@yahoo.com>
2024-03-27 22:27:46 +0800

    update owasp api 7 function

--- modules/OWASP_api7.py
+++ modules/OWASP_api7.py
@@ -1,3 +1,5 @@
 #API7:2023 - Server Side Request Forgery
 def check_api_7(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
-  pass
+  vulnerabilities = []
+  logs = []
+  return vulnerabilities, logs
