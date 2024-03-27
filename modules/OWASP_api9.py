4d0811aab025ae0e8d1e6274927bdacb4c43f3b6
rn-santos227 <rn_santos227@yahoo.com>
2024-03-27 22:28:43 +0800

    update owasp api 9 function

--- modules/OWASP_api9.py
+++ modules/OWASP_api9.py
@@ -1,3 +1,5 @@
 #API9:2023 - Improper Inventory Management
 def check_api_9(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
-  pass
+  vulnerabilities = []
+  logs = []
+  return vulnerabilities, logs
