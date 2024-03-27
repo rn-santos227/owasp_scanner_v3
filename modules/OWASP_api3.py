501761fe3c206998894daf4fc72e946aa5943668
rn-santos227 <rn_santos227@yahoo.com>
2024-03-27 19:02:34 +0800

    update owasp api 3 function

--- modules/OWASP_api3.py
+++ modules/OWASP_api3.py
@@ -1,3 +1,3 @@
 #API3:2023 - Broken Object Property Level Authorization
-def check_api_3(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None):
+def check_api_3(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
   pass
