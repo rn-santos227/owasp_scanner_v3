8c472bec40d8b09f2864492d7c63143db9909103
rn-santos227 <rn_santos227@yahoo.com>
2024-03-27 19:03:58 +0800

    update owasp api 8 function

--- modules/OWASP_api8.py
+++ modules/OWASP_api8.py
@@ -1,3 +1,3 @@
 #API8:2023 - Security Misconfiguration
-def check_api_8(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None):
+def check_api_8(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
   pass
