a3e6e91723894a910cd5d1f0d3807d6e48ea6782
rn-santos227 <rn_santos227@yahoo.com>
2024-03-27 19:02:18 +0800

    update owasp api 2 function

--- modules/OWASP_api2.py
+++ modules/OWASP_api2.py
@@ -1,3 +1,3 @@
 #API2:2023 - Broken Authentication
-def check_api_2(endpoint, method : str, headers: dict, timeout : float, verbose : bool = False, data : str = None, json : dict = None):
+def check_api_2(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
   pass
