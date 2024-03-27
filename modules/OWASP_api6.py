858250b5d7a2fd48345a2571a7db5b486b76e997
rn-santos227 <rn_santos227@yahoo.com>
2024-03-27 22:27:01 +0800

    update owasp api 6 function

--- modules/OWASP_api6.py
+++ modules/OWASP_api6.py
@@ -1,3 +1,5 @@
 #API6:2023 - Unrestricted Access to Sensitive Business Flows
 def check_api_6(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
-  pass
+  vulnerabilities = []
+  logs = []
+  return vulnerabilities, logs
