573ef5485f6b1f5612bac265b3702e22bdab12e6
rn-santos227 <rn_santos227@yahoo.com>
2024-03-28 21:29:59 +0800

    added banner in owasp api 1 function

--- modules/OWASP_api1.py
+++ modules/OWASP_api1.py
@@ -1,5 +1,8 @@
+import helpers.color_text as color
+
 #API1:2023 - Broken Object Level Authorization
 def check_api_1(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
+  color.banner("------------------------ API1:2023 - Broken Object Level Authorization ------------------------")
   vulnerabilities = []
   logs = []
   return vulnerabilities, logs
