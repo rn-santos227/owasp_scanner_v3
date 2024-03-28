5e58cf8aaf8f7c5af844882886be720691199760
rn-santos227 <rn_santos227@yahoo.com>
2024-03-28 21:34:23 +0800

    added banner in owasp api 5 function

--- modules/OWASP_api5.py
+++ modules/OWASP_api5.py
@@ -1,5 +1,8 @@
+import helpers.color_text as color
+
 #API5:2023 - Broken Function Level Authorization
 def check_api_5(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
+  color.banner("------------------------ API5:2023 - Broken Function Level Authorization ------------------------") 
   vulnerabilities = []
   logs = []
   return vulnerabilities, logs
