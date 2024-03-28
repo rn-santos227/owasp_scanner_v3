28483dee2f14251ba4b8c09738caac0cc0a2750f
rn-santos227 <rn_santos227@yahoo.com>
2024-03-28 21:30:39 +0800

    added banner in owasp api 2 function

--- modules/OWASP_api2.py
+++ modules/OWASP_api2.py
@@ -1,5 +1,8 @@
+import helpers.color_text as color
+
 #API2:2023 - Broken Authentication
 def check_api_2(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
+  color.banner("------------------------ API2:2023 - Broken Authentication ------------------------") 
   vulnerabilities = []
   logs = []
   return vulnerabilities, logs
