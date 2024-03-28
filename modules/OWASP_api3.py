74f09082b1eab91f6ed05524226b15cafbcb521e
rn-santos227 <rn_santos227@yahoo.com>
2024-03-28 21:31:10 +0800

    added banner in owasp api 3 function

--- modules/OWASP_api3.py
+++ modules/OWASP_api3.py
@@ -1,5 +1,8 @@
+import helpers.color_text as color
+
 #API3:2023 - Broken Object Property Level Authorization
 def check_api_3(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
+  color.banner("------------------------ API3:2023 - Broken Object Property Level Authorization ------------------------") 
   vulnerabilities = []
   logs = []
   return vulnerabilities, logs
