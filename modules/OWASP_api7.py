8fd35c48a2878dc989ee53c21d2e5bfe780ee7f2
rn-santos227 <rn_santos227@yahoo.com>
2024-03-29 19:53:14 +0800

    added banner in owasp api 7 function

--- modules/OWASP_api7.py
+++ modules/OWASP_api7.py
@@ -1,5 +1,8 @@
+import helpers.color_text as color
+
 #API7:2023 - Server Side Request Forgery
 def check_api_7(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
+  color.banner("------------------------ API7:2023 - Server Side Request Forgery (SSRF) ------------------------")
   vulnerabilities = []
   logs = []
   return vulnerabilities, logs
