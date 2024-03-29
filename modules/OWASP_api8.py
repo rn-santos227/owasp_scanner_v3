da9a56ebbc7e88f84946a170272bbe4154a24702
rn-santos227 <rn_santos227@yahoo.com>
2024-03-29 19:53:47 +0800

    added banner in owasp api 8 function

--- modules/OWASP_api8.py
+++ modules/OWASP_api8.py
@@ -1,5 +1,8 @@
+import helpers.color_text as color
+
 #API8:2023 - Security Misconfiguration
 def check_api_8(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
+  color.banner("------------------------ API8:2023 - Security Misconfiguration ------------------------") 
   vulnerabilities = []
   logs = []
   return vulnerabilities, logs
