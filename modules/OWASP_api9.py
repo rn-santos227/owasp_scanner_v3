3a42a810f1969934fda45d6e8dce44347cc442eb
rn-santos227 <rn_santos227@yahoo.com>
2024-03-29 19:54:26 +0800

    added banner in owasp api 9 function

--- modules/OWASP_api9.py
+++ modules/OWASP_api9.py
@@ -1,5 +1,8 @@
+import helpers.color_text as color
+
 #API9:2023 - Improper Inventory Management
 def check_api_9(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
+  color.banner("------------------------ API9:2023 - Improper Inventory Management ------------------------")
   vulnerabilities = []
   logs = []
   return vulnerabilities, logs
