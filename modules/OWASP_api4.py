1f3acef99ed301eaa47e0411201cd7df8d89d766
rn-santos227 <rn_santos227@yahoo.com>
2024-03-31 21:56:12 +0800

    update owasp api 4 scanner function

--- modules/OWASP_api4.py
+++ modules/OWASP_api4.py
@@ -1,12 +1,15 @@
 import requests
 
 import helpers.color_text as color
+from utils.validate_url import validate_url
 
 #API4:2023 - Unrestricted Resource Consumption
 def check_api_4(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
   color.banner("------------------------ API3:2023 - Broken Object Property Level Authorization ------------------------") 
   vulnerabilities = []
   logs = []
+
+  parsed_url = validate_url(endpoint)
 
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(f"API4:2023 - Unrestricted Resource Consumption")
