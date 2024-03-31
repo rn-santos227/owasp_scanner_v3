ee5a394068ffef0c4e9c4d9be244a53c49c2a825
rn-santos227 <rn_santos227@yahoo.com>
2024-03-31 21:55:13 +0800

    update owasp api 2 scanner function

--- modules/OWASP_api2.py
+++ modules/OWASP_api2.py
@@ -1,12 +1,15 @@
 import requests
 
 import helpers.color_text as color
+from utils.validate_url import validate_url
 
 #API2:2023 - Broken Authentication
 def check_api_2(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
   color.banner("------------------------ API2:2023 - Broken Authentication ------------------------") 
   vulnerabilities = []
   logs = []
+
+  parsed_url = validate_url(endpoint)
 
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(f"API2:2023 - Broken Authentication")
