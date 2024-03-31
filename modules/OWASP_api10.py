0f28a0311893486cd7b8eae91a45a1179db8abd3
rn-santos227 <rn_santos227@yahoo.com>
2024-03-31 22:01:30 +0800

    update owasp api 10 scanner function

--- modules/OWASP_api10.py
+++ modules/OWASP_api10.py
@@ -1,12 +1,15 @@
 import requests
 
 import helpers.color_text as color
+from utils.validate_url import validate_url
 
 #API10:2023 - Unsafe Consumption of APIs
 def check_api_10(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
   color.banner("------------------------ API10:2023 - Unsafe Consumption of APIs ------------------------")
   vulnerabilities = []
   logs = []
+
+  parsed_url = validate_url(endpoint)
 
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(f"API10:2023 - Unsafe Consumption of APIs")
