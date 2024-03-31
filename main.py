e5d1acabfd281d3507c06fafeaff4237bcd7627b
rn-santos227 <rn_santos227@yahoo.com>
2024-03-31 21:04:45 +0800

    added inquier choice in main.py

--- main.py
+++ main.py
@@ -4,8 +4,23 @@ import re
 import helpers.color_text as color
 from utils.print_banner import print_banner
 
+questions = [
+  inquirer.List("choice",
+    message="Choose your Activity",
+    choices=[
+      "Add an Endpoint", 
+      "Manage Endpoints Collection", 
+      "Perform Full Scan"
+      "Perform Quick Scan", 
+      "Perform Specific Scan", 
+      "Exit Program"
+    ],
+  ),
+]
+
 def main():
   color.banner(print_banner())
-  
+  answers = inquirer.prompt(questions)
+
 if __name__ == "__main__":
   main()
