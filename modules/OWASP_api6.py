b714be16770af9aaab29d813b2e940bda01412bd
rn-santos227 <rn_santos227@yahoo.com>
2024-03-31 22:00:02 +0800

    update owasp api 6 scanner function

--- modules/OWASP_api6.py
+++ modules/OWASP_api6.py
@@ -1,12 +1,15 @@
 import requests
 
 import helpers.color_text as color
+from utils.validate_url import validate_url
 
 #API6:2023 - Unrestricted Access to Sensitive Business Flows
 def check_api_6(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
   color.banner("------------------------ API6:2023 - Unrestricted Access to Sensitive Business Flows ------------------------") 
   vulnerabilities = []
   logs = []
+
+  parsed_url = validate_url(endpoint)
 
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(f"API6:2023 - Unrestricted Access to Sensitive Business Flows")
