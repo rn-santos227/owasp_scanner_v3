f42824329eff15067a2de547b5d0506fead514e1
rn-santos227 <rn_santos227@yahoo.com>
2024-03-31 22:01:08 +0800

    update owasp api 9 scanner function

--- modules/OWASP_api9.py
+++ modules/OWASP_api9.py
@@ -1,12 +1,15 @@
 import requests
 
 import helpers.color_text as color
+from utils.validate_url import validate_url
 
 #API9:2023 - Improper Inventory Management
 def check_api_9(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
   color.banner("------------------------ API9:2023 - Improper Inventory Management ------------------------")
   vulnerabilities = []
   logs = []
+
+  parsed_url = validate_url(endpoint)
 
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(f"API9:2023 - Improper Inventory Management")
