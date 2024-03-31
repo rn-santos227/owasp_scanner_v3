07524761bd66ab5b10eebd9f53cfaa787ca818f2
rn-santos227 <rn_santos227@yahoo.com>
2024-03-31 22:00:23 +0800

    update owasp api 7 scanner function

--- modules/OWASP_api7.py
+++ modules/OWASP_api7.py
@@ -1,12 +1,15 @@
 import requests
 
 import helpers.color_text as color
+from utils.validate_url import validate_url
 
 #API7:2023 - Server Side Request Forgery
 def check_api_7(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
   color.banner("------------------------ API7:2023 - Server Side Request Forgery (SSRF) ------------------------")
   vulnerabilities = []
   logs = []
+
+  parsed_url = validate_url(endpoint)
 
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(f"API7:2023 - Server Side Request Forgery (SSRF)")
