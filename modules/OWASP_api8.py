096fb8006d3fc4a261e94226591274a892856172
rn-santos227 <rn_santos227@yahoo.com>
2024-03-31 22:00:45 +0800

    update owasp api 8 scanner function

--- modules/OWASP_api8.py
+++ modules/OWASP_api8.py
@@ -1,12 +1,15 @@
 import requests
 
 import helpers.color_text as color
+from utils.validate_url import validate_url
 
 #API8:2023 - Security Misconfiguration
 def check_api_8(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
   color.banner("------------------------ API8:2023 - Security Misconfiguration ------------------------") 
   vulnerabilities = []
   logs = []
+
+  parsed_url = validate_url(endpoint)
   
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(f"API8:2023 - Security Misconfiguration")
