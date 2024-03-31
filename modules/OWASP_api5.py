31f7c414f2ad131abf91a0f93e252f07bb2247e8
rn-santos227 <rn_santos227@yahoo.com>
2024-03-31 21:56:45 +0800

    update owasp api 5 scanner function

--- modules/OWASP_api5.py
+++ modules/OWASP_api5.py
@@ -1,12 +1,15 @@
 import requests
 
 import helpers.color_text as color
+from utils.validate_url import validate_url
 
 #API5:2023 - Broken Function Level Authorization
 def check_api_5(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
   color.banner("------------------------ API5:2023 - Broken Function Level Authorization ------------------------") 
   vulnerabilities = []
   logs = []
+
+  parsed_url = validate_url(endpoint)
 
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(f"API5:2023 - Broken Function Level Authorization")
