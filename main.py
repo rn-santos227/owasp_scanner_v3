0338153b737c8af3a4e17ba2fb00e43f037b01e1
rn-santos227 <rn_santos227@yahoo.com>
2024-04-02 18:52:03 +0800

    update main.py, import select_menu

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import inquirer
 import helpers.color_text as color
 
 from helpers.menu_helper import menu
+from helpers.menu_helper import select_menu
 from utils.print_banner import print_banner
 
 def main():
