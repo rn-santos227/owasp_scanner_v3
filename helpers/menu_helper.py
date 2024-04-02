1407bdf7ebee3e4324275b08407577d4358d333b
rn-santos227 <rn_santos227@yahoo.com>
2024-04-02 19:18:29 +0800

    update menu_handler

--- helpers/menu_helper.py
+++ helpers/menu_helper.py
@@ -4,26 +4,27 @@ import sys
 menu = [
   inquirer.List("choice",
     message = "Choose your Activity",
-    choices = [
-      (1, "Add New Endpoint"),
-      (2, "Manage Endpoints Collection"),
-      (3, "Perform Full Scan"),
-      (4, "Perform Quick Scan"),
-      (5, "Perform Specific Scan"),
-      (6, "Check Bruteforce Dictionary"),
-      (7, "Check Sensitive Data Bank"),
-      (8, "Adjust Program Rate Call"),
-      (9, "Open Instruction Manual"),
-      (10, "Exit Program")
+    choices=[
+      "Add New Endpoint",
+      "Manage Endpoints Collection",
+      "Perform Full Scan",
+      "Perform Quick Scan",
+      "Perform Specific Scan",
+      "Check Bruteforce Dictionary",
+      "Check Sensitive Data Bank",
+      "Adjust Program Rate Call",
+      "Open Instruction Manual",
+      "Exit Program"
     ],
+    carousel=True
   ),
 ]
 
 def handle_choice(respond):
-  choice = respond['choice']
+  choice = respond["choice"]
   if choice == 1:
     print("You selected: Add New Endpoint")
 
-  elif choice == 10:
+  elif choice == "Exit Program":
     print("Exiting Program...")
     sys.exit()
