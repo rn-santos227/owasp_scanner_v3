7521c49919d49cb690e12be1fa1de56b6b5073f6
rn-santos227 <rn_santos227@yahoo.com>
2024-04-02 19:08:53 +0800

    update menu in menu_helper.py

--- helpers/menu_helper.py
+++ helpers/menu_helper.py
@@ -4,16 +4,16 @@ menu = [
   inquirer.List("choice",
     message = "Choose your Activity",
     choices = [
-      "Add New Endpoint", 
-      "Manage Endpoints Collection", 
-      "Perform Full Scan",
-      "Perform Quick Scan", 
-      "Perform Specific Scan",
-      "Check Bruteforce Dictionary",
-      "Check Sensitive Data Bank",
-      "Adjust Program Rate Call",
-      "Open Instruction Manual",
-      "Exit Program"
+      (1, "Add New Endpoint"),
+      (2, "Manage Endpoints Collection"),
+      (3, "Perform Full Scan"),
+      (4, "Perform Quick Scan"),
+      (5, "Perform Specific Scan"),
+      (6, "Check Bruteforce Dictionary"),
+      (7, "Check Sensitive Data Bank"),
+      (8, "Adjust Program Rate Call"),
+      (9, "Open Instruction Manual"),
+      (10, "Exit Program")
     ],
   ),
 ]
