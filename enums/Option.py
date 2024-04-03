b0faf75bc66a5a21b3415c1ea70d4661a89d1820
rn-santos227 <rn_santos227@yahoo.com>
2024-04-03 21:30:12 +0800

    added option 5 in option enums

--- enums/Option.py
+++ enums/Option.py
@@ -5,4 +5,5 @@ class Option(Enum):
   OPTION_2 = "Manage Endpoints Collection"
   OPTION_3 = "Perform Full Scan"
   OPTION_4 = "Perform Quick Scan"
+  OPTION_5 = "Perform Specific Scan"
   EXIT = "Exit Program"
