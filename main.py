32a0ca8b1c60e2d5814be3453852a60cf009e6a8
rn-santos227 <rn_santos227@yahoo.com>
2024-04-05 21:09:52 +0800

    added clear screen function in main

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import helpers.color_text as color
 
 from helpers.menu_helper import menu
 from helpers.menu_helper import handle_choice
+from utils.clear_screen import clear_screen
 from utils.print_banner import print_banner
 
 def main():
+  clear_screen()
   color.banner(print_banner())
   while True:
     user_respond = inquirer.prompt(menu)
