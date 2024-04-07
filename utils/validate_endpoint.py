37e55a17fa1b23c9479a0a932826730b828d71a1
rn-santos227 <rn_santos227@yahoo.com>
2024-04-07 20:44:27 +0800

    update validate endpoint function

--- utils/validate_endpoint.py
+++ utils/validate_endpoint.py
@@ -4,5 +4,9 @@ def validate_endpoint(endpoint_input):
   endpoint = method = token = None
   headers = {}
   timeout = None
-  
+
   parts = shlex.split(endpoint_input)
+
+  if len(parts) < 2:
+    print("[NOTICED] Invalid input. Please provide endpoint and method.")
+    return None, None, None, None, None
