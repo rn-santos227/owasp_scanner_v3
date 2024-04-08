1522c107c31fc566aace0c790b08dfb6afcbabd0
rn-santos227 <rn_santos227@yahoo.com>
2024-04-08 22:17:05 +0800

    added settings in owasp 1

--- modules/OWASP_api1.py
+++ modules/OWASP_api1.py
@@ -1,6 +1,8 @@
 import requests
 
 import helpers.color_text as color
+
+from handlers.settings import parse_config
 from utils.validate_url import validate_url
 
 #API1:2023 - Broken Object Level Authorization
