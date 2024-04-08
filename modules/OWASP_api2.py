2c91f85110b5292ea149a6e5873af29314fe93c8
rn-santos227 <rn_santos227@yahoo.com>
2024-04-08 22:17:42 +0800

    added settings in owasp 2

--- modules/OWASP_api2.py
+++ modules/OWASP_api2.py
@@ -1,6 +1,8 @@
 import requests
 
 import helpers.color_text as color
+
+from handlers.settings import parse_config
 from utils.validate_url import validate_url
 
 #API2:2023 - Broken Authentication
