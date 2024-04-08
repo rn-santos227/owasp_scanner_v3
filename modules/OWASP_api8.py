f9648d7064e986536c0f1fc730d4a0d0f0191e45
rn-santos227 <rn_santos227@yahoo.com>
2024-04-08 22:19:52 +0800

    added settings in owasp 8

--- modules/OWASP_api8.py
+++ modules/OWASP_api8.py
@@ -1,6 +1,8 @@
 import requests
 
 import helpers.color_text as color
+
+from handlers.settings import parse_config
 from utils.validate_url import validate_url
 
 #API8:2023 - Security Misconfiguration
