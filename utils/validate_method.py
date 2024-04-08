89159c08f9b5c7432c25cd8554a52278014256ad
rn-santos227 <rn_santos227@yahoo.com>
2024-04-08 22:36:18 +0800

    update validate method function

--- utils/validate_method.py
+++ utils/validate_method.py
@@ -3,5 +3,5 @@ import argparse
 def validate_method(value):
   method = value.lower()
   if method not in ['get','post','put','patch','delete']: 
-    raise argparse.ArgumentTypeError(f"argument -m/--method: invalid choice: '{method}' (choose from 'GET', 'POST', 'PATCH', 'PUT', 'DELETE')")
+    raise argparse.ArgumentTypeError(f"[NOTICE] Argument -m/--method: invalid choice: '{method}' (choose from 'GET', 'POST', 'PATCH', 'PUT', 'DELETE')")
   return method
