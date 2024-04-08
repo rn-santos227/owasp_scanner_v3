93e08e85f32cd5a3e849476a91794b16a5c78150
rn-santos227 <rn_santos227@yahoo.com>
2024-04-08 21:42:38 +0800

    added for loop for settings.conf

--- handlers/settings.py
+++ handlers/settings.py
@@ -4,3 +4,8 @@ def parse_config(filename):
   config = configparser.ConfigParser()
   config.read(filename)
   settings = {}
+
+  for section in config.sections():
+    settings[section] = {}
+    for option in config.options(section):
+      settings[section][option] = config.get(section, option)
