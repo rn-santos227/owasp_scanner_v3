d670a86aee00d6953f4346206a1920ca07f700b6
rn-santos227 <rn_santos227@yahoo.com>
2024-04-08 22:18:00 +0800

    added settings in owasp 3

--- modules/OWASP_api3.py
+++ modules/OWASP_api3.py
@@ -1,6 +1,8 @@
 import requests
 
 import helpers.color_text as color
+
+from handlers.settings import parse_config
 from utils.validate_url import validate_url
 
 #API3:2023 - Broken Object Property Level Authorization
