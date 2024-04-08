664e42a97518451be91fb11d795a2a7f136debf2
rn-santos227 <rn_santos227@yahoo.com>
2024-04-08 22:18:41 +0800

    added settings in owasp 4

--- modules/OWASP_api4.py
+++ modules/OWASP_api4.py
@@ -1,6 +1,8 @@
 import requests
 
 import helpers.color_text as color
+
+from handlers.settings import parse_config
 from utils.validate_url import validate_url
 
 #API4:2023 - Unrestricted Resource Consumption
