d6622c844e929cab4bc7f296df17b312769892e3
rn-santos227 <rn_santos227@yahoo.com>
2024-04-09 20:52:59 +0800

    update parse config function

--- handlers/settings.py
+++ handlers/settings.py
@@ -1,8 +1,11 @@
 import configparser
+import os
 
 def parse_config():
   config = configparser.ConfigParser()
-  config.read("settings.conf")
+  root_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  
+  config_path = os.path.join(root_folder, "settings.conf")  
+  config.read(config_path)
   settings = {}
 
   for section in config.sections():
