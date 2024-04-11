44c1b06a0064af04a2d22da0e8c16bb6cca380a4
rn-santos227 <rn_santos227@yahoo.com>
2024-04-11 22:13:00 +0800

    added print value in request count function in settings handler

--- handlers/settings.py
+++ handlers/settings.py
@@ -19,6 +19,7 @@ def adjust_request_count():
   pass
 
 def adjust_response_size_threshold():
+  print(f"[NOTICE] Current Value: {settings['requests_count']}")
   pass
 
 def adjust_response_time_threshold():
