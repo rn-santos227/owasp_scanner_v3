16fc403dd843af6b0461192966fe11fd15fbb9a9
rn-santos227 <rn_santos227@yahoo.com>
2024-04-12 22:55:56 +0800

    added input in adjust response size threshold function

--- handlers/settings.py
+++ handlers/settings.py
@@ -44,6 +44,7 @@ def adjust_request_count():
 def adjust_response_size_threshold():
   message = f"Current Value: {settings["response_size_threshold"]}"
   color.light_green(message)
+  new_value = input("New Value: ")
 
 def adjust_response_time_threshold():
   message = f"Current Value: {settings["response_time_threshold"]}"
