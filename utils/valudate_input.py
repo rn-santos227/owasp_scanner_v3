303fce3248982bd8d9fad686d7138af351795ffd
rn-santos227 <rn_santos227@yahoo.com>
2024-04-15 23:10:59 +0800

    update validate input function

--- utils/valudate_input.py
+++ utils/valudate_input.py
@@ -1,3 +1,7 @@
 def validate_input(data_type = "str"):
   while True:
     user_input = input(f"Please enter a Value [{data_type}]: ")
+    try:
+      break
+    except ValueError:
+      print(f"[NOTICE] Invalid input. Please enter a valid value of type {data_type}.")
