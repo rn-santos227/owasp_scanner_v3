78ed65a258db7c23ac6f703cbfffe6278f8405d7
rn-santos227 <rn_santos227@yahoo.com>
2024-04-18 22:38:33 +0800

    fixed parse config function

--- handlers/settings.py
+++ handlers/settings.py
@@ -51,8 +51,8 @@ def parse_config():
     settings[section] = config.get(section, "value")
   return settings
 
-def adjust_request_count(requests_count):
-  message = f"Current Value: {requests_count}"
+def adjust_request_count(settings):
+  message = f"Current Value: {settings[Config.CONFIG_1]}"
   color.light_green(message)
   new_value = validate_input(Type.INTEGER)
   update_config(Config.CONFIG_1, new_value)
