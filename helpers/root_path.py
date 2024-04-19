135f9487228ac4bce9b539d423dff777a433f54d
rn-santos227 <rn_santos227@yahoo.com>
2024-04-19 22:09:11 +0800

    update root path function

--- helpers/root_path.py
+++ helpers/root_path.py
@@ -1,4 +1,6 @@
 import os
 
 def root_path():
-  pass
+  root_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  
+  config_path = os.path.join(root_folder, "settings.conf")  
+  return config_path
