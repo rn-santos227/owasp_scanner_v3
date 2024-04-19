601dc955fe771ad042c9c1d07f5253b9fdd84465
rn-santos227 <rn_santos227@yahoo.com>
2024-04-19 22:01:52 +0800

    update validate input in utils

--- utils/validate_input.py
+++ utils/validate_input.py
@@ -1,10 +1,10 @@
 from classes.Type import Type 
 
-def validate_input(data_type: Type = Type.STRING):
+def validate_input(data_type: Type = Type.STRING.value):
   while True:
     user_input = input(f"Please enter a Value [{data_type}]: ")
     try:
-      if data_type == Type.INTEGER:
+      if data_type == Type.INTEGER.value:
         value = int(user_input)
       
       elif data_type == Type.FLOAT:
