84ee45499ec606670574e215f6104975ebc86033
rn-santos227 <rn_santos227@yahoo.com>
2024-04-21 22:31:58 +0800

    added owasp 4 in scanner class

--- classes/Scanner.py
+++ classes/Scanner.py
@@ -4,3 +4,4 @@ class Scanner(Enum):
   OWASP_1 = "Broken Object Level Authorization"
   OWASP_2 = "Broken Authentication"
   OWASP_3 = "Broken Object Property Level Authorization"
+  OWASP_4 = "Unrestricted Resource Consumption"
--- modules/OWASP_api4.py
+++ modules/OWASP_api4.py
@@ -7,7 +7,7 @@ from utils.validate_url import validate_url
 
 #API4:2023 - Unrestricted Resource Consumption
 def check_api_4(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
-  color.banner("------------------------ API3:2023 - Broken Object Property Level Authorization ------------------------") 
+  color.banner("------------------------ API4:2023 - Unrestricted Resource Consumption ------------------------") 
   vulnerabilities = []
   logs = []
 
