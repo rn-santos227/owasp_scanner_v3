944c8d54ae26d51dfb1095759432e5e0f4c4919b
rn-santos227 <rn_santos227@yahoo.com>
2024-04-21 22:31:05 +0800

    added owasp 3 in scanner class

--- classes/Scanner.py
+++ classes/Scanner.py
@@ -3,3 +3,4 @@ from enum import Enum
 class Scanner(Enum):
   OWASP_1 = "Broken Object Level Authorization"
   OWASP_2 = "Broken Authentication"
+  OWASP_3 = "Broken Object Property Level Authorization"
