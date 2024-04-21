1b5e1c645668c8ff947c1ff6c700d2021b10457d
rn-santos227 <rn_santos227@yahoo.com>
2024-04-21 22:44:00 +0800

    update owasp api scanner 3

--- modules/OWASP_api3.py
+++ modules/OWASP_api3.py
@@ -2,12 +2,14 @@ import requests
 
 import helpers.color_text as color
 
+from classes.Scanner import Scanner
+
 from handlers.settings import parse_config
 from utils.validate_url import validate_url
 
 #API3:2023 - Broken Object Property Level Authorization
 def check_api_3(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
-  color.banner("------------------------ API3:2023 - Broken Object Property Level Authorization ------------------------") 
+  color.banner(f"------------------------ API3:2023 - {Scanner.OWASP_2.value} ------------------------") 
   vulnerabilities = []
   logs = []
 
