a3ec139f6022d95f23021825f3b583458c1375c8
rn-santos227 <rn_santos227@yahoo.com>
2024-04-22 21:41:18 +0800

    update owasp_2 scanner in scanner list

--- classes/Scanner.py
+++ classes/Scanner.py
@@ -6,9 +6,9 @@ class Scanner:
     self.scanner_name = scanner_name
     self.function_name = function_name
 
-class ScannerEnum(Enum):
+class OWASP(Enum):
   OWASP_1 = Scanner("API1:2023", "Broken Object Level Authorization", "check_api_1")
-  # OWASP_2 = "Broken Authentication"
+  OWASP_2 = Scanner("API2:2023", "Broken Authentication", "check_api_2")
   # OWASP_3 = "Broken Object Property Level Authorization"
   # OWASP_4 = "Unrestricted Resource Consumption"
   # OWASP_5 = "Broken Function Level Authorization"
