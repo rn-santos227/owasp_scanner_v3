0b754e42d58f7b7fac328e97a29236e0b1d020ab
rn-santos227 <rn_santos227@yahoo.com>
2024-04-22 21:50:34 +0800

    update owasp api scanner 1

--- modules/OWASP_api1.py
+++ modules/OWASP_api1.py
@@ -2,14 +2,14 @@ import requests
 
 import helpers.color_text as color
 
-from classes.Scanner import Scanner
+from classes.Scanner import OWASP
 
 from handlers.settings import parse_config
 from utils.validate_url import validate_url
 
 #API1:2023 - Broken Object Level Authorization
 def check_api_1(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
-  color.banner(f"------------------------ API1:2023 - {Scanner.OWASP_1.value} ------------------------")
+  color.banner(f"------------------------ API1:2023 - {OWASP.OWASP_1.value.scanner_name} ------------------------")
   vulnerabilities = []
   logs = []
 
