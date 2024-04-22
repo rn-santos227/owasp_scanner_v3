9b4423f3b3aff6ee6481fec16ed52291b310dcb4
rn-santos227 <rn_santos227@yahoo.com>
2024-04-22 21:39:43 +0800

    update owasp_1 scanner in scanner list

--- classes/Scanner.py
+++ classes/Scanner.py
@@ -7,13 +7,13 @@ class Scanner:
     self.function_name = function_name
 
 class ScannerEnum(Enum):
-  OWASP_1 = "Broken Object Level Authorization"
-  OWASP_2 = "Broken Authentication"
-  OWASP_3 = "Broken Object Property Level Authorization"
-  OWASP_4 = "Unrestricted Resource Consumption"
-  OWASP_5 = "Broken Function Level Authorization"
-  OWASP_6 = "Unrestricted Access to Sensitive Business Flows"
-  OWASP_7 = "Server Side Request Forgery (SSRF)"
-  OWASP_8 = "Security Misconfiguration"
-  OWASP_9 = "Improper Inventory Management"
-  OWASP_10 = "Unsafe Consumption of APIs"
+  OWASP_1 = Scanner("API1:2023", "Broken Object Level Authorization", "check_api_1")
+  # OWASP_2 = "Broken Authentication"
+  # OWASP_3 = "Broken Object Property Level Authorization"
+  # OWASP_4 = "Unrestricted Resource Consumption"
+  # OWASP_5 = "Broken Function Level Authorization"
+  # OWASP_6 = "Unrestricted Access to Sensitive Business Flows"
+  # OWASP_7 = "Server Side Request Forgery (SSRF)"
+  # OWASP_8 = "Security Misconfiguration"
+  # OWASP_9 = "Improper Inventory Management"
+  # OWASP_10 = "Unsafe Consumption of APIs"
