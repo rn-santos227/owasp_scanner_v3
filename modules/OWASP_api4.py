7e72fe04d57e454439600a3d65e3eebfc86e4a54
rn-santos227 <rn_santos227@yahoo.com>
2024-04-22 21:52:40 +0800

    update owasp api scanner 4

--- modules/OWASP_api4.py
+++ modules/OWASP_api4.py
@@ -2,14 +2,14 @@ import requests
 
 import helpers.color_text as color
 
-from classes.Scanner import Scanner
+from classes.Scanner import OWASP
 
 from handlers.settings import parse_config
 from utils.validate_url import validate_url
 
 #API4:2023 - Unrestricted Resource Consumption
 def check_api_4(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
-  color.banner(f"------------------------ API4:2023 - {Scanner.OWASP_4.value} ------------------------") 
+  color.banner(f"------------------------ API4:2023 - {OWASP.OWASP_3.value.scanner_name} ------------------------") 
   vulnerabilities = []
   logs = []
 
