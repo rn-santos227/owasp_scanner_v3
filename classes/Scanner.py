6658b4f1d8fc2ff0d618f36d21d4d5c6c2812a64
rn-santos227 <rn_santos227@yahoo.com>
2024-04-23 21:31:42 +0800

    imported api 2 function to scanner class

--- classes/Scanner.py
+++ classes/Scanner.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 from modules.OWASP_api1 import check_api_1
+from modules.OWASP_api2 import check_api_2
 
 class Scanner:
   def __init__(self, scanner_id, function, scanner_name):
