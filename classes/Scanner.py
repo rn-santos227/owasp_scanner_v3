356ba43172fb2f5c6cee063cf292a6fbd1b913e0
rn-santos227 <rn_santos227@yahoo.com>
2024-04-23 21:41:44 +0800

    update owasp scanner 9 in list

--- classes/Scanner.py
+++ classes/Scanner.py
@@ -26,5 +26,5 @@ class OWASP(Enum):
   OWASP_6 = Scanner(scanner_id="API6:2023", function=check_api_6, scanner_name="Unrestricted Access to Sensitive Business Flows")
   OWASP_7 = Scanner(scanner_id="API7:2023", function=check_api_7, scanner_name="Server Side Request Forgery (SSRF)")
   OWASP_8 = Scanner(scanner_id="API8:2023", function=check_api_8, scanner_name="Security Misconfiguration")
-  OWASP_9 = Scanner("API9:2023", "Improper Inventory Management")
+  OWASP_9 = Scanner(scanner_id="API9:2023", function=check_api_9, scanner_name="Improper Inventory Management")
   OWASP_10 = Scanner("API10:2023", "Unsafe Consumption of APIs")
