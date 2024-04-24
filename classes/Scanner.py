7f3d98bff67cb928775a545e5f1fad4f58ce4090
rn-santos227 <rn_santos227@yahoo.com>
2024-04-24 20:18:42 +0800

    change scanner_id to id in scanner class

--- classes/Scanner.py
+++ classes/Scanner.py
@@ -12,19 +12,19 @@ from modules.OWASP_api9 import check_api_9
 from modules.OWASP_api10 import check_api_10
 
 class Scanner:
-  def __init__(self, scanner_id, function, scanner_name):
-    self.scanner_id = scanner_id
+  def __init__(self, id, function, scanner_name):
+    self.id = id
     self.function = function
     self.scanner_name = scanner_name
 
 class OWASP(Enum):
-  OWASP_1 = Scanner(scanner_id="API1:2023", function=check_api_1, scanner_name="Broken Object Level Authorization")
-  OWASP_2 = Scanner(scanner_id="API2:2023", function=check_api_2, scanner_name="Broken Authentication")
-  OWASP_3 = Scanner(scanner_id="API3:2023", function=check_api_3, scanner_name="Broken Object Property Level Authorization")
-  OWASP_4 = Scanner(scanner_id="API4:2023", function=check_api_4, scanner_name="Unrestricted Resource Consumption")
-  OWASP_5 = Scanner(scanner_id="API5:2023", function=check_api_5, scanner_name="Broken Function Level Authorization")
-  OWASP_6 = Scanner(scanner_id="API6:2023", function=check_api_6, scanner_name="Unrestricted Access to Sensitive Business Flows")
-  OWASP_7 = Scanner(scanner_id="API7:2023", function=check_api_7, scanner_name="Server Side Request Forgery (SSRF)")
-  OWASP_8 = Scanner(scanner_id="API8:2023", function=check_api_8, scanner_name="Security Misconfiguration")
-  OWASP_9 = Scanner(scanner_id="API9:2023", function=check_api_9, scanner_name="Improper Inventory Management")
-  OWASP_10 = Scanner(scanner_id="API10:2023", function=check_api_10, scanner_name="Unsafe Consumption of APIs")
+  OWASP_1 = Scanner(id="API1:2023", function=check_api_1, scanner_name="Broken Object Level Authorization")
+  OWASP_2 = Scanner(id="API2:2023", function=check_api_2, scanner_name="Broken Authentication")
+  OWASP_3 = Scanner(id="API3:2023", function=check_api_3, scanner_name="Broken Object Property Level Authorization")
+  OWASP_4 = Scanner(id="API4:2023", function=check_api_4, scanner_name="Unrestricted Resource Consumption")
+  OWASP_5 = Scanner(id="API5:2023", function=check_api_5, scanner_name="Broken Function Level Authorization")
+  OWASP_6 = Scanner(id="API6:2023", function=check_api_6, scanner_name="Unrestricted Access to Sensitive Business Flows")
+  OWASP_7 = Scanner(id="API7:2023", function=check_api_7, scanner_name="Server Side Request Forgery (SSRF)")
+  OWASP_8 = Scanner(id="API8:2023", function=check_api_8, scanner_name="Security Misconfiguration")
+  OWASP_9 = Scanner(id="API9:2023", function=check_api_9, scanner_name="Improper Inventory Management")
+  OWASP_10 = Scanner(id="API10:2023", function=check_api_10, scanner_name="Unsafe Consumption of APIs")
