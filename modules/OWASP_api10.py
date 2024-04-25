4e746e46b36cdca5b0fc959cd3ee07d1ddc5a3f8
rn-santos227 <rn_santos227@yahoo.com>
2024-04-25 22:22:32 +0800

    update owasp api 10 scanner function

--- modules/OWASP_api10.py
+++ modules/OWASP_api10.py
@@ -17,7 +17,7 @@ def check_api_10(endpoint, method : str, headers: dict, timeout : float, verbose
   parsed_url = validate_url(endpoint)
 
   if len(vulnerabilities) == 0:
-    endpoint_clean = color.green(f"API10:2023 - Unsafe Consumption of APIs")
+    endpoint_clean = color.green(flag_title)
     logs.append(endpoint_clean)
 
   return vulnerabilities, logs
