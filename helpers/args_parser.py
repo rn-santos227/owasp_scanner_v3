4b088a6c4134143f2ea2922c0f79cdc0ff625d47
rn-santos227 <rn_santos227@yahoo.com>
2024-04-27 21:30:18 +0800

    update args parser function

--- helpers/args_parser.py
+++ helpers/args_parser.py
@@ -6,5 +6,6 @@ from utils.validate_method import validate_method
 def parse_args():
   parser = argparse.ArgumentParser(description='Check API for broken object-level authorization.')
   parser.add_argument('endpoint', type=str, help='Endpoint URL')
-  parser.add_argument('--method', type=validate_method, required=True, help='Endpoint Method')
+  parser.add_argument('--data', type=str, action='append', help='Endpoint Data Payload (optional)')
   parser.add_argument('--header', type=str, action='append', help='Endpoint Header Configuration (optional)')
+  parser.add_argument('--method', type=validate_method, required=True, help='Endpoint Method')
