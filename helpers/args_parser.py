2be455ccff6030a5a664eaae8fdf9605fcda94eb
rn-santos227 <rn_santos227@yahoo.com>
2024-04-27 21:27:06 +0800

    update args parser function

--- helpers/args_parser.py
+++ helpers/args_parser.py
@@ -4,4 +4,4 @@ from utils.validate_headers import parse_headers
 from utils.validate_method import validate_method
 
 def parse_args():
-  pass
+  parser = argparse.ArgumentParser(description='Check API for broken object-level authorization.')
