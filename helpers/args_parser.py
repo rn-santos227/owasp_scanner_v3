d05356a14a2af8c67627d7c910e1882e08de5759
rn-santos227 <rn_santos227@yahoo.com>
2024-04-28 20:50:24 +0800

    import parse payload in args parser

--- helpers/args_parser.py
+++ helpers/args_parser.py
@@ -1,6 +1,7 @@
 import argparse
 
-from utils.validate_headers import parse_headers
+from utils.parse_headers import parse_headers
+from utils.parse_payload import parse_payload
 from utils.validate_method import validate_method
 
 def parse_args():
