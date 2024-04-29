1c04d381c029f0cab686cde7ced170001997896f
rn-santos227 <rn_santos227@yahoo.com>
2024-04-29 22:54:58 +0800

    update file reader in helpers

--- helpers/file_reader.py
+++ helpers/file_reader.py
@@ -1,7 +1,10 @@
+import helpers.color_text as color
+
 def file_reader(file : str):
   try:
     with open(file) as f:
       payloads = f.readlines()
   except Exception as err:
-    print(f"An error occurred: {err}")
+    message = color.warning(f"An error occurred: {err}")
+    print(message)
   return payloads
