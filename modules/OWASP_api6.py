af90871154085ada8884a22c979359ef6591975f
rn-santos227 <rn_santos227@yahoo.com>
2024-04-30 21:34:41 +0800

    added proxy in scanner 6

--- modules/OWASP_api6.py
+++ modules/OWASP_api6.py
@@ -2,10 +2,13 @@ import requests
 
 import helpers.color_text as color
 
+from classes.Config import Config
 from classes.Scanner import OWASP
 
 from handlers.settings import parse_config
 from utils.validate_url import validate_url
+
+proxies = parse_config()[Config.CONFIG_5.value]
 
 #API6:2023 - Unrestricted Access to Sensitive Business Flows
 def check_api_6(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
