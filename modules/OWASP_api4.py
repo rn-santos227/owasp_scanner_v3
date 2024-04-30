df9e24b2c483c6e1bd830128441a001d433141db
rn-santos227 <rn_santos227@yahoo.com>
2024-04-30 21:34:11 +0800

    added proxy in scanner 4

--- modules/OWASP_api4.py
+++ modules/OWASP_api4.py
@@ -2,11 +2,13 @@ import requests
 
 import helpers.color_text as color
 
+from classes.Config import Config
 from classes.Scanner import OWASP
 
 from handlers.settings import parse_config
 from utils.validate_url import validate_url
 
+proxies = parse_config()[Config.CONFIG_5.value]
 #API4:2023 - Unrestricted Resource Consumption
 def check_api_4(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
   flag_title = f"{OWASP.OWASP_4.value.id} - {OWASP.OWASP_4.value.name}"
