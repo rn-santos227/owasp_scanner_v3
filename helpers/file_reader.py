45477de955c8b93edfde7a6419a5f55275e8894c
rn-santos227 <rn_santos227@yahoo.com>
2024-05-02 18:44:43 +0800

    update content in helpers

--- helpers/file_reader.py
+++ helpers/file_reader.py
@@ -3,8 +3,8 @@ import helpers.color_text as color
 def file_reader(file : str):
   try:
     with open(file) as f:
-      payloads = f.readlines()
+      content = f.readlines()
   except Exception as err:
     message = color.warning(f"An error occurred: {err}")
     print(message)
-  return payloads
+  return content
