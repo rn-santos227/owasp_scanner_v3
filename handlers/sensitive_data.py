b54cc9d080288ddf32a87a7f475404af093b7478
rn-santos227 <rn_santos227@yahoo.com>
2024-05-02 19:01:20 +0800

    added sensitive data function

--- handlers/sensitive_data.py
+++ handlers/sensitive_data.py
@@ -16,3 +16,6 @@ def count_sensitive_data():
     content = file_reader("dictionary/sensitive_keys.txt")
     message = f"There are total of {len(content)} words available in sensitive keys.\n"
     color.light_green(message)
+
+def search_sensitive_data():
+  pass
