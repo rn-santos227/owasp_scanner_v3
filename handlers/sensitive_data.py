3a8ee91d86555dca7694ccbcee37bfba246c9e38
rn-santos227 <rn_santos227@yahoo.com>
2024-05-02 18:39:20 +0800

    added sensitive data menu inquirer in sensitive data handler

--- handlers/sensitive_data.py
+++ handlers/sensitive_data.py
@@ -1,3 +1,11 @@
 import inquirer
 
 from utils.clear_screen import clear_screen
+
+sensitive_data_menu = [
+  inquirer.List("choice",
+    message = "Choose your Endpoint Activity",
+    choices=[],
+    carousel=True
+  ),
+]
