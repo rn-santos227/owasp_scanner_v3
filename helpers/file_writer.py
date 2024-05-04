2edc0b71b8ee1fb2e8a99fdd15a922c074d4eea5
rn-santos227 <rn_santos227@yahoo.com>
2024-05-04 21:21:36 +0800

    update file writer in helpers

--- helpers/file_writer.py
+++ helpers/file_writer.py
@@ -4,10 +4,9 @@ import helpers.color_text as color
 
 def file_writer(file_name : str, text):
   try:
-    folder_path = "files"
-    if not os.path.exists(folder_path):
-      os.makedirs(folder_path)
-
+    create_files_folder()
+    folder_path = ""
+    
     file_path = os.path.join(folder_path, file_name)
     with open(file_path, 'a') as file:
       file.write(str(text) + '\n')
