5e71b9c5cdf0560b849d732fbce1cce61833aff6
rn-santos227 <rn_santos227@yahoo.com>
2025-02-18 14:24:21 +0800

    added endpoints file in endpoint handler

--- handlers/endpoint.py
+++ handlers/endpoint.py
@@ -7,6 +7,8 @@ from helpers.file_reader import file_reader
 from helpers.file_writer import file_writer, overwrite_file
 from utils.clear_screen import clear_screen
 
+ENDPOINTS_FILE = "files/endpoints.txt"
+
 endpoint_menu = [
   inquirer.List("choice",
     message = "Choose your Endpoint Activity",
