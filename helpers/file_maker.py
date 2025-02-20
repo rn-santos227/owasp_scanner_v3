de97d41f102fbd3df9bf97f3c111bb22dd3b542f
rn-santos227 <rn_santos227@yahoo.com>
2025-02-20 16:20:43 +0800

    updated file maker function in file maker helper

--- helpers/file_maker.py
+++ helpers/file_maker.py
@@ -3,7 +3,7 @@ import helpers.color_text as color
 
 def file_maker(file_path):
   try:
-    pass
+    folder_path = os.path.dirname(file_path)
 
   except Exception as err:
     color.warning(f"An error occurred: {err}")
