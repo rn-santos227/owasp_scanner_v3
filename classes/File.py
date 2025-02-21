d98228ae595a8c907f49b446c84bdd2e9ecf0c04
rn-santos227 <rn_santos227@yahoo.com>
2025-02-21 17:34:17 +0800

    updated file class in classes folder

--- classes/File.py
+++ classes/File.py
@@ -3,6 +3,7 @@ from enum import Enum
 class File(Enum):
   FILE_COMMANDS = "dictionaries/sql_commands_list.txt"
   FILE_ENDPOINTS = "files/endpoints.txt"
+  FILE_ERRORS = "dictionaries/sql_error_messages.txt"
   FILE_IDS = "dictionaries/test_ids.txt"
   FILE_KEYS = "dictionaries/sensitive_keys.txt"
   FILE_KEYWORDS = "dictionaries/user_keywords.txt"
