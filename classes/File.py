26ed0291704ee7aace64ccda80b29854cc3c715c
rn-santos227 <rn_santos227@yahoo.com>
2025-02-21 12:03:14 +0800

    updated file class in classes folder

--- classes/File.py
+++ classes/File.py
@@ -2,5 +2,6 @@ from enum import Enum
 
 class File(Enum):
   FILE_ENDPOINTS = "files/endpoints.txt"
-  FILE_PASSWORDS = "dictionaries/passwords.txt"
+  FILE_IDS = "dictionaries/test_ids.txt"
   FILE_KEYS = "dictionaries/sensitive_keys.txt"
+  FILE_PASSWORDS = "dictionaries/passwords.txt"
