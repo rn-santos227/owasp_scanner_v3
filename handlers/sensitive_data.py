135db6a8343f1b24bca65536c4bc6f70410362db
rn-santos227 <rn_santos227@yahoo.com>
2025-02-24 15:49:38 +0800

    updated count sensitive data function in senstive data handler

--- handlers/sensitive_data.py
+++ handlers/sensitive_data.py
@@ -22,7 +22,7 @@ sensitive_data_menu = [
 ]
 
 def count_sensitive_data():
-  content = file_reader("dictionaries/sensitive_keys.txt")
+  content = file_reader(File.FILE_KEYS)
   message = f"\nThere are total of {len(content)} words available in sensitive keys.\n"
   color.light_green(message)
 
