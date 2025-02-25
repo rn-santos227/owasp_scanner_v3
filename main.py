15bbee491eb219e10f1a1fa41dc9a2c3999e501b
rn-santos227 <rn_santos227@yahoo.com>
2025-02-25 17:11:07 +0800

    imported file class in python main file

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import inquirer
 
 import helpers.color_text as color
 
+from classes.File import File
+
 from helpers.menu_helper import menu
 from helpers.menu_helper import handle_choice
 from utils.clear_screen import clear_screen
@@ -10,6 +12,7 @@ from utils.print_banner import print_banner
 def main():
   clear_screen()
   color.banner(print_banner())
+  
   while True:
     user_respond = inquirer.prompt(menu)
     handle_choice(user_respond)
