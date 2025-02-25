0473009137d39f911749a29c8106aa22ce5fcb1c
rn-santos227 <rn_santos227@yahoo.com>
2025-02-25 17:22:51 +0800

    updated python main file

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from classes.File import File
 
 from helpers.menu_helper import menu
 from helpers.menu_helper import handle_choice
-from helpers.file_writer import file_writer
+from helpers.file_maker import file_maker
 
 from utils.clear_screen import clear_screen
 from utils.print_banner import print_banner
@@ -14,6 +14,8 @@ from utils.print_banner import print_banner
 def main():
   clear_screen()
   color.banner(print_banner())
+
+  file_maker(File.FILE_ENDPOINTS.value)
   
   while True:
     user_respond = inquirer.prompt(menu)
