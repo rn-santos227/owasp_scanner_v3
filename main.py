447ecaf5aad6fd4fe39b82cef28c65160c9c73e2
rn-santos227 <rn_santos227@yahoo.com>
2025-02-25 17:15:37 +0800

    imported file writer in python main file

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from classes.File import File
 
 from helpers.menu_helper import menu
 from helpers.menu_helper import handle_choice
+from helpers.file_writer import file_writer
+
 from utils.clear_screen import clear_screen
 from utils.print_banner import print_banner
 
