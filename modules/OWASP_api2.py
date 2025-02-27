d7e1ac68be5eb09d7fe1d2a0196298a2c34c9e1a
rn-santos227 <rn_santos227@yahoo.com>
2025-02-27 14:04:00 +0800

    updated load passwords function in owasp api 2 module

--- modules/OWASP_api2.py
+++ modules/OWASP_api2.py
@@ -16,7 +16,7 @@ def _load_test_tokens(file_path):
   return [line.strip() for line in test_tokens if line.strip()]
 
 def load_passwords(file_path):
-  pass
+  passwords = file_reader(file_path)
 
 #API2:2023 - Broken Authentication
 def check_api_2(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
