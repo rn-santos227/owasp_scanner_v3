bff3759ff073cfa06865325aa740c593ced1d8b2
rn-santos227 <rn_santos227@yahoo.com>
2025-02-27 13:45:54 +0800

    updated check api 1 function in owasp api 1 module

--- modules/OWASP_api1.py
+++ modules/OWASP_api1.py
@@ -36,6 +36,7 @@ def check_api_1(endpoint, method : str, headers: dict, timeout : float, verbose 
     try:
       response = requests.request(
         method,
+        test_endpoint,
       )
 
     except requests.RequestException as e:
