f9a881fa3b166d5cad15c2b96e8aa727c6adc579
rn-santos227 <rn_santos227@yahoo.com>
2025-03-07 10:51:21 +0800

    updated check api 2 function in owasp api2 module

--- modules/OWASP_api2.py
+++ modules/OWASP_api2.py
@@ -83,7 +83,7 @@ def check_api_2(endpoint, method : str, headers: dict, timeout : float, verbose 
       color.info("\nTesting authentication tokens...")
 
       for token in test_tokens:
-        pass
+        headers["Authorization"] = f"Bearer {token}"
 
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(flag_title)
