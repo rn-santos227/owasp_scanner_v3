b7c3cb84c100d46e157a6f80a5550dc20c22fd03
rn-santos227 <rn_santos227@yahoo.com>
2025-03-12 11:47:48 +0800

    updated check api 3 function in owasp api3 module

--- modules/OWASP_api3.py
+++ modules/OWASP_api3.py
@@ -82,7 +82,7 @@ def check_api_3(endpoint, method : str, headers: dict, timeout : float, verbose 
       color.warning(f"Error testing property '{prop}': {e}")
 
   if len(vulnerabilities) == 0:
-    endpoint_clean = color.green(flag_title)
+    endpoint_clean = color.green(f"No broken property-level authorization found at {flag_title}")
     logs.append(endpoint_clean)
 
   return vulnerabilities, logs
