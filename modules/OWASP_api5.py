b24524a147f5f81852b610c53b1ca9ec9349a4c3
rn-santos227 <rn_santos227@yahoo.com>
2025-03-13 09:54:15 +0800

    added load whitelist function in owasp api 5 module

--- modules/OWASP_api5.py
+++ modules/OWASP_api5.py
@@ -19,6 +19,9 @@ def _load_test_tokens():
   test_tokens = file_reader(_FILE_TOKENS)
   return [' '.join(word.strip() for word in line.split()) for line in test_tokens if line.strip()]
 
+def _load_whitelist():
+  pass
+
 #API5:2023 - Broken Function Level Authorization
 def check_api_5(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
   flag_title = f"{OWASP.OWASP_5.value.id} - {OWASP.OWASP_5.value.name}"
