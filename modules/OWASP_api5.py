27024e0831281b0b59464343eae7d9c7ca50bc7f
rn-santos227 <rn_santos227@yahoo.com>
2025-03-13 09:53:04 +0800

    updated load test tokens function in owasp api 5 module

--- modules/OWASP_api5.py
+++ modules/OWASP_api5.py
@@ -16,7 +16,7 @@ _FILE_TOKENS = File.FILE_TOKENS.value
 _FILE_PUBLIC_ENDPOINTS = File.FILE_WHITELIST.value
 
 def _load_test_tokens():
-  pass
+  tokens = file_reader(_FILE_TOKENS)
 
 #API5:2023 - Broken Function Level Authorization
 def check_api_5(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
