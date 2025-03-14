039970692acc80ae7bf67fbb7ff995d4db0ca64c
rn-santos227 <rn_santos227@yahoo.com>
2025-03-14 15:19:04 +0800

    updated check api 5 function in owasp api 5 module

--- modules/OWASP_api5.py
+++ modules/OWASP_api5.py
@@ -63,7 +63,7 @@ def check_api_5(endpoint, method : str, headers: dict, timeout : float, verbose 
     color.warning(f"Error during no-token request: {e}")
 
   if len(tokens) >= 2:
-    pass
+    low_privilege_token = tokens[0]
 
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(flag_title)
