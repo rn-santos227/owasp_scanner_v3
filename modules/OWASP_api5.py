56154eab43c6736785945d395b713a4fbcc93338
rn-santos227 <rn_santos227@yahoo.com>
2025-03-17 08:29:16 +0800

    updated check api 5 function in owasp api 5 module

--- modules/OWASP_api5.py
+++ modules/OWASP_api5.py
@@ -87,6 +87,9 @@ def check_api_5(endpoint, method : str, headers: dict, timeout : float, verbose 
         verify = False
       )
 
+      if low_response.status_code == 200 and high_response.status_code == 200:
+        pass
+
     except requests.RequestException as e:
       color.warning(f"Error during token-based request: {e}")
 
