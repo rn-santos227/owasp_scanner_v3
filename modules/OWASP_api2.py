5c2abb70588dbacb8fa0a7423985b7aabd137ed6
rn-santos227 <rn_santos227@yahoo.com>
2025-03-17 12:44:28 +0800

    updated check credentials function in owasp api 2 module

--- modules/OWASP_api2.py
+++ modules/OWASP_api2.py
@@ -46,7 +46,8 @@ def _check_credential(endpoint, method, headers, username, password, timeout, vu
     )
 
     if response.status_code == 200:
-      pass
+      with lock:
+        color.light_red(f"Weak credential detected: {username}:{password}")
 
   except requests.RequestException as e:
     with lock:
