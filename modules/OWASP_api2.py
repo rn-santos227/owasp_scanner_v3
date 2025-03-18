ca15177592363a23729cd6d2635330e113d43278
rn-santos227 <rn_santos227@yahoo.com>
2025-03-18 15:40:33 +0800

    updated check token function in owasp api 2 module

--- modules/OWASP_api2.py
+++ modules/OWASP_api2.py
@@ -66,7 +66,7 @@ def _check_token(endpoint: str, method: str, headers: dict, token: str, timeout:
 
   try:
     response = requests.request(
-
+      method,
     )
 
   except requests.RequestException as e:
