7f0c111080eaed00946c3e005958074183c22a46
rn-santos227 <rn_santos227@yahoo.com>
2025-03-19 19:07:55 +0800

    updated show progress bar function in progress bar utils

--- utils/progress_bar.py
+++ utils/progress_bar.py
@@ -8,4 +8,4 @@ def show_progress_bar(future_tasks, total, desc="Processing", unit="item"):
     result = future.result()
 
     for future in tqdm(as_completed(future_tasks), total=total, desc=desc, unit=unit):
-      pass
+      result = future.result()
