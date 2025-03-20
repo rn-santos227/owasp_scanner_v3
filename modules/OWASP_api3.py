ec0a7f3245992332a530c8e8edb58307687f0239
rn-santos227 <rn_santos227@yahoo.com>
2025-03-20 18:15:46 +0800

    imported show progress bar function in owap api 3 module

--- modules/OWASP_api3.py
+++ modules/OWASP_api3.py
@@ -8,6 +8,8 @@ from classes.Scanner import OWASP
 
 from handlers.settings import parse_config
 from helpers.file_reader import file_reader
+
+from utils.progress_bar import show_progress_bar 
 from utils.validate_url import validate_url
 
 proxies = parse_config()[Config.CONFIG_5.value]
