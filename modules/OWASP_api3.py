b283c7cf1c9e0c8269fdf97770dda79ab0851e3c
rn-santos227 <rn_santos227@yahoo.com>
2025-03-20 18:26:15 +0800

    updated test property batch function in owasp api 3 module

--- modules/OWASP_api3.py
+++ modules/OWASP_api3.py
@@ -43,6 +43,9 @@ def _test_property_batch(endpoint, method, headers, timeout, proxies, batch, jso
       response_json = response.json()
       vulnerabilities = []
 
+      for prop in batch:
+        pass
+
   except requests.RequestException as e:
     color.warning(f"Error testing properties batch {batch}: {e}")
 
