34544537824f643452be0d1c105c37370e018257
rn-santos227 <rn_santos227@yahoo.com>
2025-03-21 15:03:17 +0800

    updated owasp api 3 module

--- modules/OWASP_api3.py
+++ modules/OWASP_api3.py
@@ -73,55 +73,6 @@ def check_api_3(endpoint, method : str, headers: dict, timeout : float, verbose 
 
   sensitive_keys = _load_sensitive_keys()
 
-  try:
-    original_response = requests.request(
-      method,
-      parsed_url,
-      headers = headers,
-      timeout = timeout,
-      proxies = proxies,
-      verify = False
-    )
-
-  except requests.RequestException as e:
-    color.warning(f"Error fetching original data: {e}")
-    return vulnerabilities, logs
-  
-  if original_response.status_code != 200:
-    color.warning("Original request failed. Cannot perform authorization tests.")
-    return vulnerabilities, logs
-  
-  color.info("\nTesting unauthorized property access...")
-  
-  for prop in show_progress_bar(sensitive_keys, len(sensitive_keys), desc="Testing Properties", unit=" key"):
-    test_payload = json.copy() if json else {}
-    test_payload[prop] = "unauthorized_value"
-
-    try:
-      response = requests.request(
-        method,
-        parsed_url,
-        headers = headers,
-        json = test_payload,
-        timeout = timeout,
-        proxies = proxies,
-        verify = False
-      )
-
-      if response.status_code == 200 and prop in response.text:
-        color.red(f"Unauthorized access to property: '{prop}'")
-        vulnerabilities.append(f"Unauthorized access to property: {prop}")
-
-      elif response.status_code == 200 and response.json().get(prop) == "unauthorized_value":
-        color.light_red(f"Unauthorized modification of property: '{prop}'")
-        vulnerabilities.append(f"Unauthorized modification of property: {prop}")
-
-      elif verbose:
-        color.info(f"Property '{prop}' is properly secured.")
-
-    except requests.RequestException as e:
-      color.warning(f"Error testing property '{prop}': {e}")
-
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(f"No broken property-level authorization found at {flag_title}")
     logs.append(endpoint_clean)
