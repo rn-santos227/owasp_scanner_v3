29056da682e9c086cd9bf248256abbc3b1322af6
rn-santos227 <rn_santos227@yahoo.com>
2025-03-21 15:05:51 +0800

    updated check api 3 function in owasp api 3 module

--- modules/OWASP_api3.py
+++ modules/OWASP_api3.py
@@ -86,6 +86,9 @@ def check_api_3(endpoint, method : str, headers: dict, timeout : float, verbose 
   except requests.RequestException as e:
     color.warning(f"Error fetching original data: {e}")
     return vulnerabilities, logs
+  
+  if original_response.status_code != 200:
+    pass
 
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(f"No broken property-level authorization found at {flag_title}")
