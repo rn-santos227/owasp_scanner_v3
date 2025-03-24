1385092ced0a3364caefeaaff6f40d23f1cbc78a
rn-santos227 <rn_santos227@yahoo.com>
2025-03-24 13:57:15 +0800

    updated check api 4 function in owasp api 4 module

--- modules/OWASP_api4.py
+++ modules/OWASP_api4.py
@@ -57,6 +57,9 @@ def check_api_4(endpoint, method: str, headers: dict, timeout: float, verbose: b
 
   color.info("\nTesting API resource consumption...")
 
+  with ThreadPoolExecutor(max_workers=_MAX_WORKERS) as executor:
+    pass
+
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(flag_title)
     logs.append(endpoint_clean)
