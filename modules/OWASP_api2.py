d7437ad8cd7241bb6e7ea65da4542bd3fc487b8d
rn-santos227 <rn_santos227@yahoo.com>
2025-03-25 11:03:54 +0800

    updated check api 2 function in owasp api 2 module

--- modules/OWASP_api2.py
+++ modules/OWASP_api2.py
@@ -104,6 +104,8 @@ def check_api_2(endpoint, method : str, headers: dict, timeout : float, verbose 
   if not parsed_url:
     color.warning("Invalid URL. Skipping BA Test.")
     return vulnerabilities, logs
+  
+  usernames = _load_file_data(_FILE_USERNAMES)
 
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(f"No authentication vulnerabilities found at {flag_title}")
