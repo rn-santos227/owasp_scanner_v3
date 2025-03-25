1cf43104531cbb36b4c0513e8beb65055b402a15
rn-santos227 <rn_santos227@yahoo.com>
2025-03-25 11:44:14 +0800

    updated send request function in owasp api 2 module

--- modules/OWASP_api2.py
+++ modules/OWASP_api2.py
@@ -36,6 +36,7 @@ def _send_request(endpoint: str, method: str, headers: dict, timeout: int, paylo
 
   except requests.RequestException as e:
     color.warning(f"Request error: {e}")
+    return None
 
 def _check_credential(endpoint: str, method: str, headers: dict, username: str, password: str, timeout: int, verbose: bool, proxies: dict = None) -> str | None:
   auth_data = {"username": username, "password": password}
