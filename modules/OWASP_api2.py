dbe7589adad4ef2ec6476b51a31bffe2cbac7ee4
rn-santos227 <rn_santos227@yahoo.com>
2025-03-27 09:44:50 +0800

    updated check api 2 function in owasp api 2 module

--- modules/OWASP_api2.py
+++ modules/OWASP_api2.py
@@ -122,6 +122,7 @@ def check_api_2(endpoint, method : str, headers: dict, timeout : float, verbose 
 
       else:
         failed_attempts += 1
+        consecutive_successful_attempts = 0
 
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(f"No authentication vulnerabilities found at {flag_title}")
