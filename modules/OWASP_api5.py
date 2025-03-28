681e24f4cecf329d0656b1743eefd5f491f0d1bd
rn-santos227 <rn_santos227@yahoo.com>
2025-03-28 17:04:00 +0800

    updated send request function in owasp api 5 module

--- modules/OWASP_api5.py
+++ modules/OWASP_api5.py
@@ -27,6 +27,7 @@ def _send_request(endpoint: str, method: str, headers: dict, timeout: float, tok
     response = requests.request(
       method,
       endpoint,
+      headers = headers,
     )
 
   except requests.RequestException as e:
