c1983c19bdedc4aa970eda5c6e5a6cab379653a3
rn-santos227 <rn_santos227@yahoo.com>
2025-03-28 17:06:08 +0800

    updated owasp api 5 module

--- modules/OWASP_api5.py
+++ modules/OWASP_api5.py
@@ -51,7 +51,7 @@ def check_api_5(endpoint, method : str, headers: dict, timeout : float, verbose 
   public_endpoints = _load_file(_FILE_PUBLIC_ENDPOINTS)
 
   if _is_public_endpoint(endpoint, public_endpoints):
-    pass
+    color.info(f"Skipping public endpoint {endpoint} (whitelisted).")
 
 
   if len(vulnerabilities) == 0:
