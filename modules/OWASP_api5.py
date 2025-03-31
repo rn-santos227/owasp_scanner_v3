f05d66ee66a5b705873529ae7cded3fc97850a52
rn-santos227 <rn_santos227@yahoo.com>
2025-03-31 15:33:50 +0800

    updated send request function in owasp api 5 module

--- modules/OWASP_api5.py
+++ modules/OWASP_api5.py
@@ -77,6 +77,8 @@ def check_api_5(endpoint, method : str, headers: dict, timeout : float, verbose 
       (high_token, "High Privilege")
     ]
 
+    results = {}
+
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(flag_title)
     logs.append(endpoint_clean)
