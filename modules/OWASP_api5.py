7f993fd6027034c112431c6fbb532be6987d1262
rn-santos227 <rn_santos227@yahoo.com>
2025-03-31 15:35:55 +0800

    updated send request function in owasp api 5 module

--- modules/OWASP_api5.py
+++ modules/OWASP_api5.py
@@ -88,7 +88,7 @@ def check_api_5(endpoint, method : str, headers: dict, timeout : float, verbose 
       }
 
       for future in show_progress_bar(as_completed(future_tasks), len(token_tests), desc="Testing Privilege Levels", unit=" token"):
-        pass
+        role = future_tasks[future]
 
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(flag_title)
