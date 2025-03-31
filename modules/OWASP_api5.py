9a2b9ac714b17ba442b0d2ad03761ca64355279a
rn-santos227 <rn_santos227@yahoo.com>
2025-03-31 15:39:47 +0800

    updated send request function in owasp api 5 module

--- modules/OWASP_api5.py
+++ modules/OWASP_api5.py
@@ -105,6 +105,9 @@ def check_api_5(endpoint, method : str, headers: dict, timeout : float, verbose 
         color.light_red(f"Unexpected low-privilege response: Status {status_low} at {endpoint}")
         vulnerabilities.append(f"Unexpected low-privilege response at {endpoint}")
 
+      if verbose and status_high:
+        pass 
+
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(flag_title)
     logs.append(endpoint_clean)
