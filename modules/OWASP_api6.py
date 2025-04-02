ffc6edf21643bfa41ef92467cfc343a6a90eddfc
rn-santos227 <rn_santos227@yahoo.com>
2025-04-02 12:55:06 +0800

    updated send requestion function in owasp api 6 module

--- modules/OWASP_api6.py
+++ modules/OWASP_api6.py
@@ -13,10 +13,14 @@ from handlers.settings import parse_config
 from utils.validate_url import validate_url
 from utils.progress_bar import show_progress_bar
 
-def _send_request(endpoint: str, method: str, headers: dict, timeout: float, data: str = None, json: dict = None) -> tuple:
-  pass
-
 proxies = parse_config()[Config.CONFIG_5.value]
+
+def _send_request(endpoint: str, method: str, headers: dict, timeout: float, data: str = None, json: dict = None) -> tuple:
+  try:
+    pass
+
+  except requests.RequestException as e:
+    return None, str(e)
 
 #API6:2023 - Unrestricted Access to Sensitive Business Flows
 def check_api_6(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
