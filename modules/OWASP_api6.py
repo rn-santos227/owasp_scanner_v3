db1f9a1f2564e926851c235573b898d76aae4ea5
rn-santos227 <rn_santos227@yahoo.com>
2025-04-02 13:14:27 +0800

    updated check api 6 function in owasp api 6 module

--- modules/OWASP_api6.py
+++ modules/OWASP_api6.py
@@ -57,7 +57,8 @@ def check_api_6(endpoint, method : str, headers: dict, timeout : float, verbose 
       status_code, _ = future.result()
 
       if status_code == 200:
-        pass
+        success_count += 1
+
 
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(flag_title)
