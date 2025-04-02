63fbe1e3f0f9ae132b37d425f03919d327f3468d
rn-santos227 <rn_santos227@yahoo.com>
2025-04-02 12:56:23 +0800

    updated send requestion function in owasp api 6 module

--- modules/OWASP_api6.py
+++ modules/OWASP_api6.py
@@ -22,6 +22,7 @@ def _send_request(endpoint: str, method: str, headers: dict, timeout: float, dat
       endpoint,
       headers = headers,
       timeout = timeout,
+      proxies = proxies,
     )
 
   except requests.RequestException as e:
