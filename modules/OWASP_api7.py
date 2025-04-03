88417a1b2af9d62057d53ce158f587a7e6343630
rn-santos227 <rn_santos227@yahoo.com>
2025-04-03 15:07:14 +0800

    added send ssrf request function in owasp api 7 module

--- modules/OWASP_api7.py
+++ modules/OWASP_api7.py
@@ -19,6 +19,8 @@ _SSRF_TEST_URLS = File.FILE_URLS.value
 def _load_file(file_path: str) -> list[str]:
   return [' '.join(line.strip().split()) for line in file_reader(file_path) if line.strip()]
 
+def _send_ssrf_request(endpoint: str, method: str, headers: dict, timeout: float, test_url: str) -> tuple[str, int | None]:
+  pass
 
 #API7:2023 - Server Side Request Forgery
 def check_api_7(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
