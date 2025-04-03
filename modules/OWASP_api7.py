0d0738432f7668764fa3f5de66d7b113678317e4
rn-santos227 <rn_santos227@yahoo.com>
2025-04-03 15:05:34 +0800

    added load file function in owasp api 7 module

--- modules/OWASP_api7.py
+++ modules/OWASP_api7.py
@@ -7,6 +7,7 @@ from classes.File import File
 from classes.Scanner import OWASP
 
 from handlers.settings import parse_config
+from helpers.file_reader import file_reader
 
 from utils.validate_url import validate_url
 from utils.progress_bar import show_progress_bar
@@ -14,6 +15,10 @@ from utils.progress_bar import show_progress_bar
 proxies = parse_config()[Config.CONFIG_5.value]
 
 _SSRF_TEST_URLS = File.FILE_URLS.value
+
+def _load_file(file_path: str) -> list[str]:
+  return [' '.join(line.strip().split()) for line in file_reader(file_path) if line.strip()]
+
 
 #API7:2023 - Server Side Request Forgery
 def check_api_7(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
