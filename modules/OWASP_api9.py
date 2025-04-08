be26a93549c85872b398cdd92a460857cdc75351
rn-santos227 <rn_santos227@yahoo.com>
2025-04-08 16:18:03 +0800

    updated check api 9 function in owasp api 9 module

--- modules/OWASP_api9.py
+++ modules/OWASP_api9.py
@@ -54,7 +54,10 @@ def check_api_9(endpoint, method : str, headers: dict, timeout : float, verbose 
 
   total_tasks = len(sensitive_paths) + len(backup_suffixes) + len(uncommon_methods)
   completed_tasks = 0
-  
+
+  with ThreadPoolExecutor(max_workers=10) as executor:
+    future_to_path = {}
+
   if len(vulnerabilities) == 0:
     endpoint_clean = color.green(flag_title)
     logs.append(endpoint_clean)
