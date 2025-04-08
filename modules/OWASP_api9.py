ef246e9b49620764ed7081209c981c82367239c6
rn-santos227 <rn_santos227@yahoo.com>
2025-04-08 16:13:08 +0800

    updated send request function in owasp api 9 module

--- modules/OWASP_api9.py
+++ modules/OWASP_api9.py
@@ -28,6 +28,7 @@ def _send_request(endpoint: str, method: str, headers: dict, timeout: float, pat
     url = f"{endpoint}/{path}" if path else endpoint
     response = requests.request(
       method,
+      url,
     )
 
   except requests.RequestException as e:
