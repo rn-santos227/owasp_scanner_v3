697fd0cf88edfdabd2f83c86e490177b24f304d5
rn-santos227 <rn_santos227@yahoo.com>
2025-04-10 10:31:28 +0800

    updated check api 5 function in owasp api 5 module

--- modules/OWASP_api_05.py
+++ modules/OWASP_api_05.py
@@ -43,7 +43,7 @@ def _send_request(endpoint: str, method: str, headers: dict, timeout: float, tok
     return None
 
 #API5:2023 - Broken Function Level Authorization
-def check_api_5(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
+def check_api_05(endpoint, method : str, headers: dict, timeout : float, verbose : bool, data : str = None, json : dict = None, response = None):
   flag_title = f"{OWASP.OWASP_5.value.id} - {OWASP.OWASP_5.value.name}"
   color.banner(f"------------------------ {flag_title} ------------------------")
   vulnerabilities = []
