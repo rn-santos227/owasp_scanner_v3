74957322f0c42714ca66d62b046c445beb87bea1
rn-santos227 <rn_santos227@yahoo.com>
2025-04-14 08:05:55 +0800

    updated scanner file in handlers folder

--- handlers/scanner.py
+++ handlers/scanner.py
@@ -10,7 +10,9 @@ from utils.validate_url import validate_url
 
 _FILE_ENDPOINTS = File.FILE_ENDPOINTS.value
 
-def _choose_endpoint() -> str:
+def _choose_endpoint() -> str | None:
   endpoints = file_reader(_FILE_ENDPOINTS)
   if not endpoints:
     color.warning("No endpoints available to scan.")
+    input("Press Enter to Continue...")
+    return None
