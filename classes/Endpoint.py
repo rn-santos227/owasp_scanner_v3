7cdde4e26fd621648200f4cb3f17290cd99d5d00
rn-santos227 <rn_santos227@yahoo.com>
2025-04-15 11:30:47 +0800

    updated constructor in endpoint class

--- classes/Endpoint.py
+++ classes/Endpoint.py
@@ -4,9 +4,11 @@ class Endpoint:
     url: str,
     method: str = "GET",
     headers: dict = None,
+    timeout: float = 10.0,
     verbose: bool = False,        
   ):
     self.url = url
     self.method = method.upper()
     self.headers = headers if headers else {}
+    self.timeout = timeout
     self.verbose = verbose
