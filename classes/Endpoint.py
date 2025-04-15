f66c06cfe645fde4ca6d077a94f3e678f630b0b3
rn-santos227 <rn_santos227@yahoo.com>
2025-04-15 11:29:52 +0800

    updated constructor in endpoint class

--- classes/Endpoint.py
+++ classes/Endpoint.py
@@ -2,7 +2,9 @@ class Endpoint:
   def __init__(
     self,
     url: str,
+    method: str = "GET",
     headers: dict = None,         
   ):
     self.url = url
     self.method = method.upper()
+    self.headers = headers if headers else {}
