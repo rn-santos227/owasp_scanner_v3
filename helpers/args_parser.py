424f4260b100f98e12a7e005d0ec5bed35454c41
rn-santos227 <rn_santos227@yahoo.com>
2025-04-23 14:30:09 +0800

    updated get parsed args in args parser helper

--- helpers/args_parser.py
+++ helpers/args_parser.py
@@ -12,3 +12,4 @@ def get_parsed_args(raw_input: str):
   parser.add_argument('endpoint', type=str, help='Endpoint URL')
   parser.add_argument('-m', '--method', type=validate_method, required=True, help='HTTP method')
   parser.add_argument('-h', '--header', type=str, action='append', help='Header(s) (key:value)')
+  parser.add_argument('--data', type=str, action='append', help='Form or query data')
