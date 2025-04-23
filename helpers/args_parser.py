c2cbe2eeca00db7165a1c39eabb35b8f8a80a711
rn-santos227 <rn_santos227@yahoo.com>
2025-04-23 14:30:47 +0800

    updated get parsed args in args parser helper

--- helpers/args_parser.py
+++ helpers/args_parser.py
@@ -15,3 +15,4 @@ def get_parsed_args(raw_input: str):
   parser.add_argument('--data', type=str, action='append', help='Form or query data')
   parser.add_argument('--payload', type=str, action='append', help='JSON payload')
   parser.add_argument('-t', '--timeout', type=float, default=10.0, help='Request timeout')
+  parser.add_argument('-v', '--verbose', action='store_true', help='Verbose output')
