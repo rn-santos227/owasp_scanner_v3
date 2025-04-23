d5f96836938cdc80bf029fe0e2b9b5ad0c93e8dd
rn-santos227 <rn_santos227@yahoo.com>
2025-04-23 14:29:38 +0800

    updated get parsed args in args parser helper

--- helpers/args_parser.py
+++ helpers/args_parser.py
@@ -10,3 +10,4 @@ def get_parsed_args(raw_input: str):
 
   parser = argparse.ArgumentParser(description='Parse structured endpoint input')
   parser.add_argument('endpoint', type=str, help='Endpoint URL')
+  parser.add_argument('-m', '--method', type=validate_method, required=True, help='HTTP method')
