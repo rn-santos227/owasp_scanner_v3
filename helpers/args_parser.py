cde3f1fed773d0ee14e8e87dccbca98465b33688
rn-santos227 <rn_santos227@yahoo.com>
2025-04-23 14:27:39 +0800

    added get parsed args in args parser helper

--- helpers/args_parser.py
+++ helpers/args_parser.py
@@ -4,17 +4,5 @@ from utils.parse_headers import parse_headers
 from utils.parse_payload import parse_payload
 from utils.validate_method import validate_method
 
-def parse_args():
-  parser = argparse.ArgumentParser(description='Check API for broken object-level authorization.')
-  parser.add_argument('endpoint', type=str, help='Endpoint URL')
-  parser.add_argument('--data', type=str, action='append', help='Endpoint Data Query (optional)')
-  parser.add_argument('--header', type=str, action='append', help='Endpoint Header Configuration (optional)')
-  parser.add_argument('--payload', type=str, action='append', help='Endpoint Data Payload (optional)')
-  parser.add_argument('--method', type=validate_method, required=True, help='Endpoint Method')
-  parser.add_argument('--timeout', type=int, help='Endpoint Timeout')
-
-  args = parser.parse_args()
-  headers_dict = parse_headers(args.header)
-  payload_dict = parse_payload(args.payload)
-
-  return args, headers_dict, payload_dict
+def get_parsed_args(raw_input: str):
+  pass
