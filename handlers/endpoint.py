39078159448e4c7d73c4ac07616a966314a3eb25
rn-santos227 <rn_santos227@yahoo.com>
2025-04-25 15:52:33 +0800

    updated read endpoint function in endpoint handler

--- handlers/endpoint.py
+++ handlers/endpoint.py
@@ -45,6 +45,7 @@ def _read_endpoint():
 
   if endpoints:
     choices = {f"{i + 1}. {line.strip()}": line.strip() for i, line in enumerate(endpoints)}
+    question = [inquirer.List("selected", message="Select an endpoint to view details", choices=list(choices.keys()))]
 
 def _update_endpoint():
   endpoints = file_reader(_FILE_ENDPOINTS)
