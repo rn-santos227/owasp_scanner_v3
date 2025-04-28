ca79ca645f8fd9fe768571e53eaf4055e1282362
rn-santos227 <rn_santos227@yahoo.com>
2025-04-28 16:16:15 +0800

    updated read endpoint function in endpoint handler

--- handlers/endpoint.py
+++ handlers/endpoint.py
@@ -46,6 +46,9 @@ def _create_endpoint():
 def _read_endpoint():
   endpoints = file_reader(_FILE_ENDPOINTS)
 
+  if not endpoints:
+    pass
+
   choices = {f"{i + 1}. {line.strip()}": line.strip() for i, line in enumerate(endpoints)}
   question = [inquirer.List("selected", message="Select an endpoint to view details", choices=list(choices.keys()))]
   answer = inquirer.prompt(question)
