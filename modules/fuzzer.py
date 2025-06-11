4ccb960cac07269fd8e6be1a8394f7deffad3511
rn-santos227 <rn_santos227@yahoo.com>
2025-06-11 20:25:35 +0800

    updated fuzzer file in modules

--- modules/fuzzer.py
+++ modules/fuzzer.py
@@ -9,3 +9,5 @@ from classes.File import File
 from handlers.settings import parse_config
 from helpers.file_reader import file_reader
 from utils.validate_url import validate_url
+
+proxies = parse_config()[Config.CONFIG_5.value]
