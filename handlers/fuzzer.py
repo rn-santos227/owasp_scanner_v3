12a59a44a6f9b25b60ac3382ab525873903a4c7d
rn-santos227 <rn_santos227@yahoo.com>
2025-06-11 20:24:26 +0800

    created fuzzer file in handler

---
+++ handlers/fuzzer.py
@@ -0,0 +1,9 @@
+import inquirer
+import helpers.color_text as color
+
+from classes.File import File
+from helpers.file_reader import file_reader
+from helpers.file_writer import file_writer
+
+from utils.clear_screen import clear_screen
+from utils.validate_url import validate_url
